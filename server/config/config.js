0ff42ddc2dc56add67a9cac099e7e5440c9c334e
Rafael Rodriguez <rarodriguez@mardom.com>
2020-04-09 13:13:51 -0400

    edit config

--- server/config/config.js
+++ server/config/config.js
@@ -7,6 +7,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //DataBase
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') urlDB = 'mongodb://localhost:27017/cafe';
-urlDB = 'mongodb+srv://cafeDB_admin:cafe123@cafe-aw74i.mongodb.net/test?retryWrites=true&w=majority';
+if (process.env.NODE_ENV === 'dev') urlDB = 'mongodb://localhost:27017/cafe';
+else urlDB = 'mongodb+srv://cafeDB_admin:cafe123@cafe-aw74i.mongodb.net/test?retryWrites=true&w=majority';
 process.env.URLDB = urlDB;
